edb46fff1d3924415e65b137e82b0f6da2d4bc65
Anton Whalley <anton@venshare.com>
2020-06-09 22:36:16 +0100

    use environment variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,12 @@ git clone https://github.com/${GITHUB_REPOSITORY}.git
 REPO_NAME=$(echo ${GITHUB_REPOSITORY} | cut -d/ -f2-)
 cd ${REPO_NAME}
 git reset --hard $GITHUB_SHA
-DOCKER_ID=$3
-echo $4 | docker login --username $DOCKER_ID --password-stdin
-IMAGE_NAME=${DOCKER_ID}/${REPO_NAME}:${GITHUB_SHA}
+echo $DOCKER_API_KEY | docker login --username $REGISTRY_USERNAME --password-stdin
+IMAGE_NAME=${REGISTRY_USERNAME}/${REPO_NAME}:${GITHUB_SHA}
 appsody build -t ${IMAGE_NAME} --push
 IBMCLOUD_VERSION_CHECK=false
-IBMCLOUD_API_KEY=$2
-ibmcloud coligo target --name $1
+IBMCLOUD_API_KEY=$IBMCLOUD_API_KEY
+ibmcloud coligo target --name $PROJECT
 ibmcloud coligo application create --name ${REPO_NAME} --image ${IMAGE_NAME}
 time=$(date)
 echo "::set-output name=time::$time"
