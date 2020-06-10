3b12275563fa317da37116b0521c655be1602ad9
Anton Whalley <anton@venshare.com>
2020-06-10 02:18:27 +0100

    use api key login

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ ibmcloud plugin install container-registry
 ibmcloud plugin install kubernetes-service
 ibmcloud plugin install coligo
 ibmcloud plugin show coligo
-ibmcloud login --apikey $IBM_CLOUD_API_KEY -r us-south
+ibmcloud login -r us-south
 ibmcloud coligo target --name $PROJECT
 ibmcloud coligo application create --name ${REPO_NAME} --image ${IMAGE_NAME}
 time=$(date)
